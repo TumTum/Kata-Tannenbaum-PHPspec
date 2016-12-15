9cd06d1627c54b962a24f4cd6bc9f16ad86e45c2
Tobias Matthaiou <developer@tobimat.eu>
2016-12-15 08:12:51 +0100

    Fram refactoriert um es schneller zu lesen

--- src/application/TreeService.php
+++ src/application/TreeService.php
@@ -19,14 +19,36 @@ class TreeService
 
     public function farm(TreeTypeInterface $treeType)
     {
+        $this->buildTop($treeType);
+        $this->buildTreecrown($treeType);
+        $this->buildTrunk($treeType);
+    }
+
+    /**
+     * @param TreeTypeInterface $treeType
+     */
+    private function buildTop(TreeTypeInterface $treeType): void
+    {
         foreach ($treeType->createTop() as $line) {
             $this->outputStream->addLine($line);
         }
+    }
 
+    /**
+     * @param TreeTypeInterface $treeType
+     */
+    private function buildTreecrown(TreeTypeInterface $treeType): void
+    {
         for ($i = 2; $i <= $treeType->getHeight(); $i++) {
             $this->outputStream->addLine($treeType->createLine($i));
         }
+    }
 
+    /**
+     * @param TreeTypeInterface $treeType
+     */
+    private function buildTrunk(TreeTypeInterface $treeType): void
+    {
         $this->outputStream->addLine($treeType->createTrunk());
     }
 }
